36e91b6807ab7d956799cbe13c39ddf969d04037
jessicahattig <jessicahattig@gmail.com>
2023-08-19 17:39:57 -0700

    BUGFIX: Seperate BI and UI logic.

--- src/currency.js
+++ src/currency.js
@@ -1,23 +1,12 @@
 export default class CurrencyConverter {
-  static async getExchangeRate(amount = 1, fromCurrency = 'USD', toCurrency = 'EUR') {
+  static async getExchangeRate(amount = 1, toCurrency = 'EUR') {
+    const apiKey = process.env.API_KEY;
     try {
-      const apiKey = process.env.API_KEY;
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/USD/${toCurrency}/${amount}`);
       const jsonifiedResponse = await response.json();
-
-      if (jsonifiedResponse.result === 'error') {
-        const errorMessage = `API Error: ${jsonifiedResponse['error-type']}`;
-        throw new Error(errorMessage);
-      }
-
-      if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}`;
-        throw new Error(errorMessage);
-      }
-
       return jsonifiedResponse;
     } catch (error) {
-      return error;
+      return { result: 'error', 'error-type': error.message };
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,34 +5,28 @@ import CurrencyConverter from './currency.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const currencyForm = document.querySelector('#currencyForm');
-  const amountInput = document.querySelector('#amount');
-  const currencySelect = document.querySelector('#currency');
   const resultDiv = document.querySelector('#result');
 
   currencyForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent form submission to avoid page reload
 
+    const amountInput = document.querySelector('#amount');
+    const currencySelect = document.querySelector('#currency');
     const amount = parseFloat(amountInput.value);
     const currency = currencySelect.value;
 
-    if (isNaN(amount) || amount <= 0 || amount > 1000000) {
-      resultDiv.textContent = 'Please enter a valid amount up to 1 million USD.';
-      return;
-    }
+    const exchangeRateData = await CurrencyConverter.getExchangeRate(amount, currency);
 
-    try {
-      const exchangeRateData = await CurrencyConverter.getExchangeRate(amount, 'USD', currency);
-
-      if (exchangeRateData['result'] === 'error') {
-        resultDiv.textContent = exchangeRateData['error-type'];
-      } else {
-        const targetRate = exchangeRateData.conversion_rate;
-        const convertedAmount = (amount * targetRate).toFixed(2);
-
-        resultDiv.textContent = `${amount} USD is approximately ${convertedAmount} ${currency}`;
-      }
-    } catch (error) {
-      resultDiv.textContent = 'An error occurred while fetching the exchange rate.';
-    }
+    displayResult(exchangeRateData, amount, currency, resultDiv);
   });
 });
+
+function displayResult(data, amount, currency, resultDiv) {
+  if (data.result === 'error') {
+    resultDiv.textContent = data['error-type'];
+  } else {
+    const targetRate = data.conversion_rate;
+    const convertedAmount = (amount * targetRate).toFixed(2);
+    resultDiv.textContent = `${amount} USD is approximately ${convertedAmount} ${currency}`;
+  }
+}
