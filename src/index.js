1327f1fc15d526fcea63a781709dea9614f4e934
jessicahattig <jessicahattig@gmail.com>
2023-08-19 17:27:22 -0700

    BUGFIX: exchange rate.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './currency.js';
 
-// Get references to DOM elements
-const currencyForm = document.querySelector('#currencyForm');
-const amountInput = document.querySelector('#amount');
-const currencySelect = document.querySelector('#currency');
-const resultDiv = document.querySelector('#result');
+document.addEventListener('DOMContentLoaded', () => {
+  const currencyForm = document.querySelector('#currencyForm');
+  const amountInput = document.querySelector('#amount');
+  const currencySelect = document.querySelector('#currency');
+  const resultDiv = document.querySelector('#result');
 
-// Event listener for the form submission
-currencyForm.addEventListener('submit', async (event) => {
-  event.preventDefault(); // Prevent form submission to avoid page reload
+  currencyForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevent form submission to avoid page reload
 
-  const amount = parseFloat(amountInput.value);
-  const currency = currencySelect.value;
+    const amount = parseFloat(amountInput.value);
+    const currency = currencySelect.value;
 
-  if (isNaN(amount) || amount <= 0 || amount > 1000000) {
-    resultDiv.textContent = 'Please enter a valid amount up to 1 million USD.';
-    return;
-  }
-
-  try {
-    const exchangeRateData = await CurrencyConverter.getExchangeRate(amount, 'USD', currency);
-
-    if (exchangeRateData['result'] === 'error') {
-      resultDiv.textContent = exchangeRateData['error-type'];
-    } else {
-      const targetRate = exchangeRateData.conversion_rate;
-      const convertedAmount = (amount * targetRate).toFixed(2);
-
-      resultDiv.textContent = `${amount} USD is approximately ${convertedAmount} ${currency}`;
+    if (isNaN(amount) || amount <= 0 || amount > 1000000) {
+      resultDiv.textContent = 'Please enter a valid amount up to 1 million USD.';
+      return;
     }
-  } catch (error) {
-    resultDiv.textContent = 'An error occurred while fetching the exchange rate.';
-  }
+
+    try {
+      const exchangeRateData = await CurrencyConverter.getExchangeRate(amount, 'USD', currency);
+
+      if (exchangeRateData['result'] === 'error') {
+        resultDiv.textContent = exchangeRateData['error-type'];
+      } else {
+        const targetRate = exchangeRateData.conversion_rate;
+        const convertedAmount = (amount * targetRate).toFixed(2);
+
+        resultDiv.textContent = `${amount} USD is approximately ${convertedAmount} ${currency}`;
+      }
+    } catch (error) {
+      resultDiv.textContent = 'An error occurred while fetching the exchange rate.';
+    }
+  });
 });
